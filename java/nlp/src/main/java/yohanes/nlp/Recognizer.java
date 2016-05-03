2564864b89071457b2eb6160c1645bc02b5d2830
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-05-03 13:32:27 +0700

    refactor

--- java/nlp/src/main/java/yohanes/nlp/Recognizer.java
+++ java/nlp/src/main/java/yohanes/nlp/Recognizer.java
@@ -1,24 +1,82 @@
 package yohanes.nlp;
 
-import opennlp.tools.namefind.NameFinderME;
-import opennlp.tools.namefind.NameSample;
-import opennlp.tools.namefind.NameSampleDataStream;
-import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.namefind.*;
 import opennlp.tools.util.ObjectStream;
 import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.Span;
+import opennlp.tools.util.TrainingParameters;
+import opennlp.tools.util.featuregen.*;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by yohanes on 23/04/16.
  */
 public class Recognizer {
 
-    public void convertTrainFile(String inputFilePath, String outFilePath) throws Exception {
+    private TokenNameFinderModel model;
+    private NameFinderME nameFinder;
+
+    public Recognizer(TokenNameFinderModel newModel) {
+        this.model = newModel;
+        this.nameFinder = new NameFinderME(this.model);
+    }
+
+    public Recognizer(String trainFilepath, String lang, String name) throws Exception {
+        this.train(trainFilepath, lang, name);
+    }
+
+    public Recognizer(String trainFilepath, String lang, String name, int type) throws Exception {
+        switch (type) {
+            case 1:
+                this.train1(trainFilepath, lang, name);
+                break;
+            default:
+                this.train(trainFilepath, lang, name);
+        }
+    }
+
+    public void setModel(TokenNameFinderModel newModel) {
+        this.model = newModel;
+        if (this.nameFinder == null || !this.model.equals(newModel)) {
+            this.nameFinder = new NameFinderME(this.model);
+        }
+    }
+
+    private void train(String trainFilepath, String lang, String name) throws Exception {
+        ObjectStream<String> lineStream = new PlainTextByLineStream(new FileInputStream(trainFilepath), Charset.forName("UTF-8"));
+        ObjectStream<NameSample> sampleStream = new NameSampleDataStream(lineStream);
+        TokenNameFinderModel newModel = NameFinderME.train(lang, name, sampleStream, Collections.<String, Object>emptyMap());
+        this.setModel(newModel);
+    }
+
+    private void train1(String trainFilepath, String lang, String name) throws Exception {
+        ObjectStream<String> lineStream = new PlainTextByLineStream(new FileInputStream(trainFilepath), Charset.forName("UTF-8"));
+        ObjectStream<NameSample> sampleStream = new NameSampleDataStream(lineStream);
+        AdaptiveFeatureGenerator featureGenerator = new CachedFeatureGenerator(
+            new AdaptiveFeatureGenerator[]{
+                    new WindowFeatureGenerator(new TokenFeatureGenerator(), 2, 2),
+                    new WindowFeatureGenerator(new TokenClassFeatureGenerator(true), 2, 2),
+                    new OutcomePriorFeatureGenerator(),
+                    new PreviousMapFeatureGenerator(),
+                    new BigramNameFeatureGenerator(),
+                    new SentenceFeatureGenerator(true, false)
+            }
+        );
+        TokenNameFinderModel newModel = NameFinderME.train(lang, name, sampleStream, TrainingParameters.defaultParams(), featureGenerator, Collections.<String, Object>emptyMap());
+        this.setModel(newModel);
+    }
+
+    public Span[] find(String[] tokens) {
+        return this.nameFinder.find(tokens);
+    }
+
+    public static void convertTrainFile(String inputFilePath, String outFilePath) throws Exception {
         File outTrainFile = new File(outFilePath);
         // if file does not exists, then create it
         if (!outTrainFile.exists()) {
@@ -48,34 +106,36 @@ public class Recognizer {
 
     }
 
+    public static List<String> spansToList(Span[] spans, String[] tokens) {
+        ArrayList<String> res = new ArrayList<String>();
+        for (Span sp:spans) {
+            String text = "";
+            for (int i = sp.getStart(); i < sp.getEnd(); i++) {
+                if (i > sp.getStart()) {
+                    text += " ";
+                }
+                text += tokens[i];
+            }
+            res.add(text + "\t" + sp.getType());
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        Charset charset = Charset.forName("UTF-8");
         String trainFilepath = (args.length >= 1) ? args[0] : "train.txt";
         String sentence[] = new String[]{"Samsung", "mendarat", "di", "Bandara", "Halim", "." };
         ObjectStream<String> lineStream = null;
         ObjectStream<NameSample> sampleStream = null;
         try {
-            Recognizer app = new Recognizer();
             String trainDir = trainFilepath.substring(0, trainFilepath.lastIndexOf(File.separator) + 1);
             String trainName = trainFilepath.substring(trainFilepath.lastIndexOf(File.separator) + 1, trainFilepath.lastIndexOf("."));
             String convertedTrainFilepath = trainDir + trainName + ".train" ;
-            app.convertTrainFile(trainFilepath, convertedTrainFilepath);
+            Recognizer.convertTrainFile(trainFilepath, convertedTrainFilepath);
 
-            lineStream = new PlainTextByLineStream(new FileInputStream(convertedTrainFilepath), charset);
-            sampleStream = new NameSampleDataStream(lineStream);
-            TokenNameFinderModel model = NameFinderME.train("id", "train", sampleStream, Collections.<String, Object>emptyMap());
-            NameFinderME nfm = new NameFinderME(model);
-            Span nameSpans[] = nfm.find(sentence);
-            for (Span sp:nameSpans) {
-                String text = "";
-                for (int i = sp.getStart(); i < sp.getEnd(); i++) {
-                    if (i > sp.getStart()) {
-                        text += " ";
-                    }
-                    text += sentence[i];
-                }
-                System.out.println(text+ " " + sp.getType());
-            }
+            Recognizer recognizer = new Recognizer(convertedTrainFilepath, "id", "news.id", 1);
+            Span[] nameSpans = recognizer.find(sentence);
+            System.out.println(Recognizer.spansToList(nameSpans, sentence));
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
